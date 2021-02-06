74a853bc7e66d2bf590489919830e6e8c4b2207a
Théophile Dano <theo.dano@gmail.com>
2021-02-06 16:18:45 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,10 +15,10 @@
                         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
                             <div>
                                 <label for="about" class="block text-sm font-medium text-gray-700">
-                                    Code <span class="font-bold">avec</span> prefix
+                                    Code <span class="font-bold">without</span> prefix
                                 </label>
                                 <div class="mt-1">
-                                    <textarea id="about" name="code"  rows="15" class="shadow-sm focus:ring-green-500 focus:border-green-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="<div ...">{{ $code ?? null }}</textarea>
+                                    <textarea id="about" name="code"  rows="15" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="<div ...">{{ $code ?? null }}</textarea>
                                 </div>
                             </div>
                         </div>
@@ -29,10 +29,10 @@
                         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
                             <div>
                                 <label for="about" class="block text-sm font-medium text-gray-700">
-                                    Code <span class="font-bold">sans</span> prefix
+                                    Code <span class="font-bold">with</span> prefix :)
                                 </label>
                                 <div class="mt-1">
-                                    <textarea readonly rows="15" class="shadow-sm focus:ring-green-500 focus:border-green-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md">{{ $formatedCode ?? null }}</textarea>
+                                    <textarea readonly rows="15" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md">{{ $formatedCode ?? null }}</textarea>
                                 </div>
                             </div>
                         </div>
@@ -50,7 +50,7 @@
                                 <input type="text" name="prefix" id="email" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300" value="tw-">
                             </div>
                             <button type="submit"  class="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-                                <span>Transformer</span>
+                                <span>Transform</span>
                             </button>
                         </div>
                     </div>
